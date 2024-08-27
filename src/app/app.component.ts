4ceae7a77b75f862bcde34f7206d5f2daef2dee5
Yassmine <yassmineelmir5@gmail.com>
2024-08-28 00:29:04 +0100

    logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,52 @@
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
+  imports: [RouterModule, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ShopEasy';
+  userName: string = '';
+  userEmail: string = '';  // Add this if you want to display the email
+  isUserMenuOpen = false;
 
+  constructor(
+    private keycloakService: KeycloakService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.initKeycloak();
+    }
   }
 
-  
+  private async initKeycloak(): Promise<void> {
+    const isLoggedIn = await this.keycloakService.isLoggedIn();
+    if (isLoggedIn) {
+      try {
+        this.userName = this.keycloakService.getUsername();
+        const userProfile = await this.keycloakService.loadUserProfile();
+        this.userEmail = userProfile.email || ''; // Retrieve the email from the profile
+      } catch (error) {
+        console.error('Failed to load user profile:', error);
+      }
+    } else {
+      console.warn('User not logged in');
+    }
+  }
 
+  logout(): void {
+    this.keycloakService.logout();
+  }
+
+  toggleUserMenu(): void {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+}
